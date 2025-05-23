9c5f335ff44e1c95c0661cc1fe904cecb9dba1db
Priya Chanchal <145918494+Priya-C-016@users.noreply.github.com>
2025-05-23 20:24:10 +0530

    Create IterativeInorder.cpp

---
+++ IterativeInorder.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+    int data;
+    Node* left,*right;
+    Node(int val){
+        data=val;
+        left=right=NULL;
+    }
+};
+Node* insert(Node* root, int val){
+    if(root==NULL){
+        return new Node(val);
+    }
+    if(val<root->data){
+        root->left=insert(root->left,val);
+    }else{
+        root->right=insert(root->right,val);
+    }
+    return root;
+}
+void iterativeInorder(Node* root){
+    stack<Node*>st;
+    Node* curr=root;
+    while(curr!=NULL || !st.empty() ){
+        while(curr!=NULL){
+            st.push(curr);
+            curr=curr->left;
+        }
+        curr=st.top();st.pop();
+        cout<<curr->data<<" ";
+        curr=curr->right;
+    }
+}
+int main(){
+    Node* root=NULL;
+    root=insert(root,5);
+    insert(root,3);
+    insert(root,8);
+    insert(root,6);
+    iterativeInorder(root);
+    return 0;
+}
