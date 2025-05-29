334fc645b3ae95e3ec8fd6fb662c03a2e6f9f041
Priya Chanchal <145918494+Priya-C-016@users.noreply.github.com>
2025-05-29 20:54:13 +0530

    Create KthLargest.cpp

---
+++ KthLargest.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+    int data;
+    Node* left,*right;
+    Node(int val){
+        data=val;
+        left=right=NULL;
+    }
+};
+Node* insert(Node* root,int val){
+    if(root==NULL){
+        return new Node(val);
+    }
+    if(val<root->data){
+        root->left=insert(root->left,val);
+    }else{
+        root->right=insert(root->right,val);
+    }
+    return root;
+}
+
+int KthLargest(Node* root,int &k){
+    if(root==NULL)return -1;
+    int right=KthLargest(root->right,k);
+    if(right!=-1)return right;
+    k--;
+    if(k==0)return root->data;
+    return KthLargest(root->left,k);
+    
+    
+}
+
+void print(Node* root){
+    if(root==NULL)return;
+    print(root->left);
+    cout<<root->data<<" ";
+    print(root->right);
+}
+
+int main(){
+    Node* root=NULL;
+    root=insert(root,3);
+    root=insert(root,5);
+    root=insert(root,2);
+    root=insert(root,8);
+    root=insert(root,6);
+    root=insert(root,9);
+    print(root);
+    int k=2;
+    cout<<"\n"<<KthLargest(root,k);
+    return 0;
+}
